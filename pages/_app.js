dc9722f10b3cde0a95a3e008bc4ba530efb16325
AlecCross <cross2472@gmail.com>
2025-03-05 19:26:32 +0000

    New Config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import './globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then((registration) => console.log('SW registered:', registration))
+          .catch((error) => console.error('SW registration failed:', error));
+      });
+    }
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
