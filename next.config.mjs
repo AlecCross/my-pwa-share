841d3b6d22051b4b70fdf9cdd33424dd7077cb85
AlecCross <cross2472@gmail.com>
2025-03-05 14:36:43 +0000

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,10 @@
-import withPWA from "next-pwa";
+import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // вимикає PWA у dev
-  runtimeCaching: [
-    {
-      urlPattern: /.*\.(png|jpg|jpeg|webp|svg|gif)/,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "image-cache",
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 днів
-        },
-      },
-    },
-  ]
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
 });
 
-export default {
-  ...nextConfig,
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" }
-        ],
-      },
-    ];
-  },
-};
+export default nextConfig;
